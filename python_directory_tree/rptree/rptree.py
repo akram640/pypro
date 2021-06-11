7f35d8edaae1712afac204a7c1d80008b6e4edca
Akram Faiz <akramfaiz84@gmail.com>
2021-06-11 18:02:40 +0530

    need to add argparse

--- python_directory_tree/rptree/rptree.py
+++ python_directory_tree/rptree/rptree.py
@@ -14,7 +14,7 @@ class DirectoryTree:
     def __init__(self,root_dir):
         self._generator = _TreeGenerator(root_dir)
 
-    def generator(self):
+    def generate(self):
         tree = self._generator.build_tree()
         for entry in tree:
             print(entry)
@@ -33,3 +33,36 @@ class _TreeGenerator:
     def _tree_head(self):
         self._tree.append(f"{self._root_dir}{os.sep}")
         self._tree.append(PIPE)
+
+    def _tree_body(self,directory,prefix=""):
+        entries = directory.iterdir()
+        entreis = sorted(entries, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(entry,index,entries_count,prefix,connector)
+            else:
+                self._add_file(entry,prefix,connector)
+
+
+
+    def _add_directory(self,directory,index,entries_count,prefix,connector):
+        self._tree.append(f"{prefix}{connector} {directory.name}{os.sep}") 
+        if index != entries_count - 1:
+            prefix += PIPE_PREFIX
+        else:
+            prefix += SPACE_PREFIX
+        self._tree_body(directory=directory,prefix=prefix)
+        self._tree.append(prefix.rstrip())
+
+        def _add_file(self,file,prefix,connector):
+            self._tree.append(f"{prefix}{connecotr} {file.name}")
+
+
+
+
+
+
+

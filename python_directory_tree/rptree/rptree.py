a2c9db28f0ad52246102c1e0bc840997ee57a7b3
Akram Faiz <akramfaiz84@gmail.com>
2021-06-11 16:59:55 +0530

    created classes for higher and low level tree

--- python_directory_tree/rptree/rptree.py
+++ python_directory_tree/rptree/rptree.py
@@ -7,3 +7,29 @@ ELBOW="|__"
 TEE="|--"
 PIPE_PREFIX = "|    "
 SPACE_PREFIX = "    "
+
+# high lavel directory tree class
+
+class DirectoryTree:
+    def __init__(self,root_dir):
+        self._generator = _TreeGenerator(root_dir)
+
+    def generator(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
+
+# low lavel tree generator class
+class _TreeGenerator:
+    def __init__(self, root_dir):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def build_tree(self):
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
